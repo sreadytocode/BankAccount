9da86d554d53c550b12e54facbe78e4eaaaef973
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-12 18:40:36 +0000

    Wrote first code for first transaction test and got it to pass

--- src/main/java/com/example/bankaccount/models/bankaccounts/Transaction.java
+++ src/main/java/com/example/bankaccount/models/bankaccounts/Transaction.java
@@ -1,4 +1,34 @@
 package com.example.bankaccount.models.bankaccounts;
 
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "transactions")
 public class Transaction {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column
+    private String date;
+
+    public Transaction(String date) {
+        this.date = date;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDate() {
+        return date;
+    }
+
+    public void setDate(String date) {
+        this.date = date;
+    }
 }
--- src/test/java/com/example/bankaccount/TransactionTest.java
+++ src/test/java/com/example/bankaccount/TransactionTest.java
@@ -1,5 +1,6 @@
 package com.example.bankaccount;
 
+import com.example.bankaccount.models.bankaccounts.Transaction;
 import org.junit.Before;
 import org.junit.Test;
 
