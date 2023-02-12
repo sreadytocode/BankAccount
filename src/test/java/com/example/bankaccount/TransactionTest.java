a365586ada0c48b71d9daac7837facdb78369196
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-12 18:37:10 +0000

    Created new java class for testing Transaction wrote first test

---
+++ src/test/java/com/example/bankaccount/TransactionTest.java
@@ -0,0 +1,20 @@
+package com.example.bankaccount;
+
+import jakarta.transaction.Transaction;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.assertEquals;
+
+public class TransactionTest {
+    Transaction transaction;
+
+    @Before
+    public void before(){
+        transaction = new Transaction("12/02/2023")
+    }
+    @Test
+    public void TransactionHasDate(){
+        assertEquals("12/02/2023", transaction.getDate())
+    }
+}
