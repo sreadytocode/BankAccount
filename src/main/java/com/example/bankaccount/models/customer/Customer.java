fea5623ff0f6761610bd26fb3f2e5d62a777b76a
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-12 17:00:16 +0000

    Refactured code as to remove person class as unnecessary

--- src/main/java/com/example/bankaccount/models/customer/Customer.java
+++ src/main/java/com/example/bankaccount/models/customer/Customer.java
@@ -1,12 +1,86 @@
 package com.example.bankaccount.models.customer;
 
 import com.example.bankaccount.models.allenums.Title;
+import jakarta.persistence.*;
 
-public class Customer extends Person{
+@Entity
+@Table(name = "customers")
+public class Customer {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    Title title;
+    @Column
+    private String firstName;
+    @Column
+    private String lastName;
+    @Column
+    private int age;
+    @Column
+    private String address;
+
+    @Column
     private double monthlyWage;
-    public Customer(Title title, String firstName, String lastName, int age, String address, Double monthlyWage) {
-        super(title, firstName, lastName, age, address);
+
+    public Customer(Title title, String firstName, String lastName, int age, String address, double monthlyWage) {
+        this.title = title;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.age = age;
+        this.address = address;
         this.monthlyWage = monthlyWage;
+    }
+
+    public Customer() {}
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Title getTitle() {
+        return title;
+    }
+
+    public void setTitle(Title title) {
+        this.title = title;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public int getAge() {
+        return age;
+    }
+
+    public void setAge(int age) {
+        this.age = age;
+    }
+
+    public String getAddress() {
+        return address;
+    }
+
+    public void setAddress(String address) {
+        this.address = address;
     }
 
     public double getMonthlyWage() {
