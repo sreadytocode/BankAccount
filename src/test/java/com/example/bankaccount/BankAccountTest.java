8efa2102e91f8d662b25e6b5b2ba7db13e281f0d
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-12 23:53:47 +0000

    Wrote next test

--- src/test/java/com/example/bankaccount/BankAccountTest.java
+++ src/test/java/com/example/bankaccount/BankAccountTest.java
@@ -1,9 +1,13 @@
 package com.example.bankaccount;
 
 import com.example.bankaccount.models.allenums.Card;
+import com.example.bankaccount.models.allenums.Type;
 import com.example.bankaccount.models.bankaccounts.BankAccount;
+import com.example.bankaccount.models.bankaccounts.Transaction;
 import org.junit.Before;
 import org.junit.Test;
+
+import java.util.ArrayList;
 
 import static org.junit.Assert.assertEquals;
 
@@ -12,27 +16,38 @@ public class BankAccountTest {
 
     @Before
     public void before(){
-        bankAccount = new BankAccount(639268, "11-23-54", Card.VISA, 1000.0);
+        Transaction transaction1 = new Transaction("12/02/2023", 14.00, Type.INCOMING, "Calanthe", 50.0, "TOKEEPTHEPEACE");
+        Transaction transaction2 = new Transaction("05/10/2023", 10.00, Type.INCOMING, "Bloody Baron", 50.0, "FAMILYMATTERS");
+        ArrayList<Transaction> transactions = new ArrayList<>();
+        transactions.add(transaction1);
+        transactions.add(transaction2);
+
+        bankAccount = new BankAccount(639268, "11-23-54", Card.VISA, 1000.0, transactions);
     }
 
     @Test
-    public void BankAccountHasAccountNumber(){
+    public void doesBankAccountHaveAnAccountNumber(){
         assertEquals(639268, bankAccount.getAccountNumber(), 0.0);
     }
 
     @Test
-    public void BankAccountHasSortCode(){
+    public void doesBankAccountHaveASortCode(){
         assertEquals("11-23-54", bankAccount.getSortCode());
     }
 
     @Test
-    public void BankAccountHasCard(){
+    public void doesBankAccountHaveACard(){
         assertEquals(Card.VISA, bankAccount.getCard());
     }
 
     @Test
-    public void BankAccountHasCash(){
+    public void doesBankAccountHaveCash(){
         assertEquals(1000.00, bankAccount.getCash(), 0.0);
+    }
+
+    @Test
+    public void doesBankAccountHaveTransactions(){
+        assertEquals(2, bankAccount.getTransactionCount());
     }
 
 
