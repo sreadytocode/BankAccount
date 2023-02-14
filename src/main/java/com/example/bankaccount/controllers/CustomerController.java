346319532b2e14a3f63199ff367a77c07fb97e1e
sara.younis2303 <sara.younis2303@gmail.com>
2023-02-14 00:22:41 +0000

    Finished CRUD functionality for controllers for customer class

--- src/main/java/com/example/bankaccount/controllers/CustomerController.java
+++ src/main/java/com/example/bankaccount/controllers/CustomerController.java
@@ -5,10 +5,7 @@ import com.example.bankaccount.repositories.CustomerRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 
@@ -27,5 +24,30 @@ public class CustomerController {
     @GetMapping(value = "/customers/{id")
     public ResponseEntity getRecipe(@PathVariable Long id) {
         return new ResponseEntity<>(customerRepository.findById(id), HttpStatus.OK);
+    }
+
+    @PostMapping(value = "/customers")
+    public ResponseEntity<Customer> postCustomer(@RequestBody Customer customer){
+        customerRepository.save(customer);
+        return new ResponseEntity<>(customer, HttpStatus.CREATED);
+    }
+
+    @PutMapping(value = "/customers/{id}")
+    public ResponseEntity<Customer> putCustomer(@RequestBody Customer customer, @PathVariable Long id){
+        Customer customerToUpdate = customerRepository.findById(id).get();
+        customerToUpdate.setTitle(customer.getTitle());
+        customerToUpdate.setFirstName(customer.getFirstName());
+        customerToUpdate.setLastName(customer.getLastName());
+        customerToUpdate.setAge(customer.getAge());
+        customerToUpdate.setAddress(customer.getAddress());
+        customerToUpdate.setMonthlyWage(customer.getMonthlyWage());
+        customerToUpdate.setBankAccounts(customer.getBankAccounts());
+        return new ResponseEntity<>(customerToUpdate, HttpStatus.OK);
+    }
+
+    @DeleteMapping(value = "/customers/{id}")
+    public ResponseEntity<Long> deleteCustomer(@PathVariable Long id) {
+        customerRepository.deleteById(id);
+        return new ResponseEntity<>(id, HttpStatus.OK);
     }
 }
